7e5dae84a21284d8d453a2a0b2ca0c57bdf78f9d
ParkHeeseung <gmltmd4493@gmail.com>
2018-12-19 23:39:49 +0900

    3046

---
+++ BaekJoon/3046.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  int R1, S;
+  cin >> R1 >> S;
+
+  cout << S * 2 - R1 << endl;
+
+  return 0;
+
+}
