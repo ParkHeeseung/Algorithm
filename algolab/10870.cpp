a1234ce90cd1ea23196307547dac5987d854dd0e
ParkHeeseung <gmltmd4493@gmail.com>
2020-03-19 09:44:40 +0900

    10870

---
+++ algolab/10870.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int func(int n);
+int main() {
+
+  int n;
+  cin >> n;
+
+  cout << func(n) << endl;
+
+
+  
+}
+
+int func(int n) {
+  
+  if (n == 0) return 0;
+  else if (n == 1) return 1;
+
+  return func(n - 1) + func(n - 2);
+}
